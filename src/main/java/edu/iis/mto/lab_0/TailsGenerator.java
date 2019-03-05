4a7aae08ed272384a170a15303d6f32e49b8ab6d
Ciosek.Natalia <n_ciosek@wp.pl>
2019-03-05 18:11:41 +0100

    Testy

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,12 +1,25 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+
+        if(value == null) {
+           return Collections.emptyList();
+        }
+
+        List<String> arrayList = new ArrayList<>();
+
+        for(int i = 0; i < value.length(); i++) {
+            String temp = value.substring(i, value.length());
+            arrayList.add(temp);
+        }
+        arrayList.add("");
+        return arrayList;
     }
 
 }
